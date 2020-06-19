5669bf9db24e10f56817221de8b084534d4444e8
soyuzbek <soyuzbek196.kg@gmail.com>
2020-06-19 19:26:11 +0600

    added comments & docs

--- script.py
+++ script.py
@@ -9,15 +9,21 @@ IN_DIR = os.path.join('.', 'input-images')
 
 
 def image_handle(input_dir, output_dir):
+    """
+    Put copyright sign to images within given directory.
+    :param input_dir: directory which contains images to be signed.
+    :param output_dir: new images' directory of processed images.
+    :return: None
+    """
     font = ImageFont.truetype('arial', 32)
-    for file in os.listdir(input_dir):
+    for file in os.listdir(input_dir):  # walking through source input directory
         if file.endswith(".jpg"):
             im = Image.open(os.path.join(input_dir, file))
             width, height = im.size
             first_name, last_name = file[:-4].split('-')
             img_draw = ImageDraw.Draw(im)
-            sign = f'\N{COPYRIGHT SIGN}{first_name.capitalize()} {last_name.capitalize()}'
-            sign_width, sign_height = img_draw.textsize(sign, font)
+            sign = f'\N{COPYRIGHT SIGN}{first_name.capitalize()} {last_name.capitalize()}'  # generating the sign text
+            sign_width, sign_height = img_draw.textsize(sign, font)  # required to align sign bottom-right.
             img_draw.text((width - sign_width - 20, height - sign_height - 15), sign, (255, 255, 255), font)
             im.save(os.path.join(output_dir, file))
 
